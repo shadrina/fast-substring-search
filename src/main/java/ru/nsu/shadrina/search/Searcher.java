6e94456a31d018c212c65cc53b863e9c41d1e1b6
Anastasiya Shadrina <a.shadrina5@mail.ru>
2019-11-13 02:17:25 +0700

    Add algorithms implementation from Algorithms, 4th Edition by Robert Sedgewick and Kevin Wayne

--- src/main/java/ru/nsu/shadrina/search/Searcher.java
+++ src/main/java/ru/nsu/shadrina/search/Searcher.java
@@ -1,8 +1,13 @@
 package ru.nsu.shadrina.search;
 
+import java.io.File;
+import java.util.Arrays;
+
 public class Searcher {
     public static void searchByName(String name, String folderPath) {
-
+        File dir = new File(folderPath);
+        File[] files = dir.listFiles((dir1, fileName) -> fileName.equals(name));
+        System.out.println(Arrays.toString(files));
     }
 
     public static void searchByText(String text, String folderPath) {
